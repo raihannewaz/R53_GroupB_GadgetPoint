06cb28634372136b819a5dc3b75d8abaf85f8001
raihannewaz <raihan_rn02@yahoo.com>
2023-09-15 18:46:38 +0600

    extra field removed

--- R53_GroupB_GadgetPoint/DTOs/ProductDTO.cs
+++ R53_GroupB_GadgetPoint/DTOs/ProductDTO.cs
@@ -6,7 +6,6 @@ namespace R53_GroupB_GadgetPoint.DTOs
     {
 
         public int ProductId { get; set; }
-        public string ProdcutName { get; set; }
         public string ProductName { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
